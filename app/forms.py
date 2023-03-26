78b45194fa053070956e2760a440c4a3183eda03
Harshwardhan More <87003053+HarshwardhanMore@users.noreply.github.com>
2023-03-26 11:21:45 +0530

    done

--- app/forms.py
+++ app/forms.py
@@ -8,4 +8,5 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         # fields = ('username', 'email', 'password1', 'password2')
-        fields = ('first_name', 'last_name', 'username','email', 'password1', 'password2')
+        # fields = ('first_name', 'last_name', 'username', 'email',  'password1', 'password2')
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
